07260c6e34bdf0d5bf2cc692b171781d7fa8acbd
techjawir <67239767+techjawir@users.noreply.github.com>
2020-06-23 18:55:28 +0700

    Initial Upload
    go02_outputPrint
    Output dari program perlu perbaikan. 
    Semua tips untuk perbaikan ada video https://youtu.be/7mid-pK5onU

---
+++ go02_outputPrint/go02_outputPrint.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nama:", "TechJawir1", "\nUmur:", 27)
+	fmt.Println("--------------------------------")
+	fmt.Print("Nama: ", "TechJawir2", "\nUmur: ", 28)
+	fmt.Println("--------------------------------")
+	fmt.Printf("Nama: %s Umur: %v\n", "TechJawir3", 29)
+}
