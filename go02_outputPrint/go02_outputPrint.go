ec9272532da85b3162ea8fc06ab0aa41a58756c6
techjawir <67239767+techjawir@users.noreply.github.com>
2020-06-23 20:17:13 +0700

    Add Output Sampel
    Output program ini masih ada yang salah, dan perlu perbaikan.
    Output yang di harapkan:
    
    Nama: TechJawir1
    Umur: 27
    --------------------------------
    Nama: TechJawir2
    Umur: 28
    --------------------------------
    Nama: TechJawir3 
    Umur: 29
    
    Semua tips utk perbaikan ada di video https://youtu.be/7mid-pK5onU

--- go02_outputPrint/go02_outputPrint.go
+++ go02_outputPrint/go02_outputPrint.go
@@ -1,6 +1,17 @@
 package main
 /*
 Output program ini masih ada yang salah, dan perlu perbaikan.
+Output yang di harapkan:
+
+Nama: TechJawir1
+Umur: 27
+--------------------------------
+Nama: TechJawir2
+Umur: 28
+--------------------------------
+Nama: TechJawir3 
+Umur: 29
+
 Semua tips utk perbaikan ada di video https://youtu.be/7mid-pK5onU
 */
 
